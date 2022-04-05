a947699361ee99b42406c40f0dad2d5f4ae3649b
dusky1ight <33296087+dusky1ight@users.noreply.github.com>
2022-04-05 16:34:02 +0530

    updatation

--- home-login/scripts.js
+++ home-login/scripts.js
@@ -60,12 +60,12 @@ function onSignIn(googleUser) {
     $("#email").text(profile.getEmail());
     $("#image").attr('src',profile.getImageurl());
     $(".data").css("display","block");
-    $(".g-signin2").css("display","none");
+    $(".g-signin2").css("display","block");
 
-    // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    // console.log('Name: ' + profile.getName());
-    // console.log('Image URL: ' + profile.getImageUrl());
-    // console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
   }
   
     function signOut() {
