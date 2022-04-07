d9feba766da7343ad4719f0dc5a2052a0264d7d9
dusky1ight <33296087+dusky1ight@users.noreply.github.com>
2022-04-07 16:44:50 +0530

    localstorage

--- patari/kitaab.js
+++ patari/kitaab.js
@@ -1,5 +1,3 @@
-'use strict';
-
 let pRecord = [];
 // example {id:1592304983049, title: 'Deadpool', year: 2015}
 const addMovie = (ev)=>{
@@ -14,26 +12,17 @@ const addMovie = (ev)=>{
             pRecord.push(patientRecord);
             document.forms[0].reset(); // to clear the form for the next entries
             //document.querySelector('form').reset();
-            let data = JSON.stringify(patientRecord);  
-            const fs = require('fs');
-            fs.writeFileSync('file.json', data, finished);
-            
-            function finished(err)
-            {
-                console.log('success');
-            }
-            //for display purposes only
-//             console.warn('added' , {pRecord} );
-//             let pre = document.querySelector('#msg pre');
-            
-//             pre.textContent = '\n' + JSON.stringify(pRecord, '\t',10);
 
-//             //saving to localStorage
-//             localStorage.setItem('aj ka record',JSON.stringify(pRecord));
-// //            localStorage.setItem('MyMovieList', JSON.stringify(pRecord) );
+            //for display purposes only
+            console.warn('added' , {pRecord} );
+            let pre = document.querySelector('#msg pre');
+            
+            pre.textContent = '\n' + JSON.stringify(pRecord, '\t',10);
+
+            //saving to localStorage
+            localStorage.setItem('aj ka record',JSON.stringify(pRecord));
+//            localStorage.setItem('MyMovieList', JSON.stringify(pRecord) );
         }
-    document.addEventListener('DOMContentLoaded', ()=>
-    {
-        document.getElementById('btn').addEventListener('click', addMovie);
-    }
-    );
+        document.addEventListener('DOMContentLoaded', ()=>{
+            document.getElementById('btn').addEventListener('click', addMovie);
+        });
